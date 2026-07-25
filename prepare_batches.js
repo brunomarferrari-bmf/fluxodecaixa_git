b79e22a425490e3eb2ac9aaca71da070d371d530
Bruno <brunomartinsferrari@gmail.com>
2026-07-25 00:17:37 -0300

    refactor: remove deprecated PDF parsing and SQL batch generation scripts

--- prepare_batches.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const batches = JSON.parse(fs.readFileSync('sql_categorized_batches.json', 'utf8'));
-
-batches.forEach((sql, idx) => {
-  fs.writeFileSync(`cat_batch_${idx}.sql`, sql);
-});
-
-console.log(`Saved ${batches.length} batch files.`);
